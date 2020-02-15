c304dd8f70618d5d44742e79374f5c037eb1a64e
Jalal Uddin <jalal.exe@gmail.com>
2020-02-15 10:15:27 +0600

    added arithmetic operations

--- hello.php
+++ hello.php
@@ -1,2 +1,50 @@
 <?php
-echo "Hello from Jalaluddin";
+
+$x = 5;
+$y = 10;
+$z = $x + $y;
+
+$city = "ঢাকা";  // string
+$country = "Bangladesh";
+$name = "Jalaluddin";
+
+$_year = 2020; // integer
+$price = 23.50; // float or double
+$present = true; // boolean
+$today = date("d"); // date 
+
+$year = 2020;
+$year = "new year";
+$year = 2020.5;
+$year = false; // syntax
+
+// this is a comment 
+
+/* 
+this is a big
+big 
+big comment
+*/
+
+$z = $x + $y; // addition
+$z = $x - $y; // subtraction
+$z = $x * $y; // multiplication
+$z = $x / $y; // division
+$z = $x % $y; // modulo
+$z = $x ** $y; //exponentiation
+
+$z++; // $z = $z + 1; Increament operator
+$z--; // $z = $z - 1; decreament operation
+++$z;
+--$z;
+
+$z = 7;
+$a = $z++; // 7
+
+echo "a = $a, z = $z \n";
+
+$z = 7;
+$a = ++$z;
+echo "a = $a, z = $z \n";
+
+
